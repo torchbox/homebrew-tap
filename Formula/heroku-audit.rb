64fb8977f305db723fd84835004ba46f690c3c2b
Jake Howard <jake.howard@torchbox.com>
2023-09-07 15:19:25 +0100

    Ensure `pip` is in venv

--- Formula/heroku-audit.rb
+++ Formula/heroku-audit.rb
@@ -10,7 +10,7 @@ class HerokuAudit < Formula
   depends_on "python@3"
 
   def install
-    venv  = virtualenv_create(libexec, "python3")
+    venv = virtualenv_create(libexec, "python3", without_pip: false)
     system libexec/"bin/pip", "install", buildpath
     system libexec/"bin/pip", "uninstall", "-y", "heroku-audit"
     venv.pip_install_and_link buildpath
